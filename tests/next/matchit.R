2caee4c1ea844046c206c60149d9d36e4a35b375
RichaArora <richa.arora@fuzzylogix.com>
2016-11-17 07:18:15 -0500

    matchit

--- tests/next/matchit.R
+++ tests/next/matchit.R
@@ -1,22 +1,9 @@
 #matchit function
 tab <- FLTable("tblMatchitAlt","PERSON_ID")
-# #do a in-db logistic regression (glm) ree test_glm_logistic.R as a reference 
-# 2. scoring: combine scores and obsid, treatment and (optional) MatchOrderCol in a view/temp table (create a FLTable object for this) 
-# 3. determine a temp output table name (see createTable and gen_unique_table_name functions for reference ) 
-# 4. call FLMatchit 
-# 5. constructing the result 
-# 1. call: see lm implementation 
-# 2. set formula to argument 
-# 3. set model to result of glm logistic regression 
-# 4. set match.matrix to NA 
-# 5. set discarded to FLVector with matchit results 
-# 6. set distance to NA 
-# 7. set weights to NA 
-# 8. set subclass an q.cut to same value as R matchit when doing nearest neighbor 
-# 9. set treat,X to formula parts (see lm implementation for ref) 
-# 10. reconstruct nn after instpecting R matchit results. 
+sqlQuery(connection,"show table tblMatchitAlt")
 
 require(MatchIt)
+
 Renv = new.env(parent = globalenv())
 
 logitinv <- function(l) exp(l)/(1+exp(l))
@@ -27,54 +14,80 @@ Renv$dataf <- dataf
 FLenv <- as.FL(Renv)
 
 dataf<-FLenv$dataf
-#Forml denotes formula
-Forml <- var3 ~ var1+var2
-fit <- glm(F,data=dataf, family="binomial")
+## 1. do a in-db logistic regression (glm) ree test_glm_logistic.R as a reference 
+FORM <- var3 ~ var1+var2
+fit <- glm(FORM,data=dataf, family="binomial")
+summary(fit)
 
-#predict
-options(debugSQL = TRUE)
-head(predict(fit,type="response"))
-head(logitinv(predict(fit)))
-score<-predict(fit)
+# 2. scoring: combine scores and obsid, treatment and (optional) MatchOrderCol in a view/temp table (create a FLTable object for this) 
+outWideTable <- gen_view_name("regprep")
+ret <- sqlStoredProc(connection,
+                     "FLLogRegrScore",
+                     TableName = fit@deeptable@select@table_name,
+                     ObsIDCol = fit@deeptable@select@variables$obs_id_colname,
+                     VarIDCol = fit@deeptable@select@variables$var_id_colname,
+                     ValueCol = fit@deeptable@select@variables$cell_val_colname,
+                     WhereClause = "NULL",
+                     RegrAnalysisID = fit@AnalysisID,
+                     ScoreTable = outWideTable,
+                     Note = "Matchit scoring table",
+                     outputParameter = c(OutTable = 'a')
+)
 
-predict.matchit <- function(object,
-                              newdata=object@table,
-                              scoreTable=""){
-  return(predict.lmGeneric(object,newdata=newdata,
-                           scoreTable=scoreTable))
-}
+# 3. determine a temp output table name (see createTable and gen_unique_table_name functions for reference ) and amend with exposure col
+getDepVarname <- function(fit) rownames(attr(terms(fit@formula),"factors"))[[1]]
+getObsIDColColname <- function(dataf) dataf@select@variables$obs_id_colname # getIndexSQLExpression(dataf,1)
+getTable <- function(dataf) dataf@select@table_name
+
+sqlSendUpdate(connection,paste0("alter table ",outWideTable," add exposure INTEGER"))
+sqlSendUpdate(connection,paste0("update a from ",outWideTable," a, ",getTable(dataf), " b\n",
+                                " set exposure=b.",getDepVarname(fit),"\n",
+                                " where a.",getObsIDColColname(fit@deeptable),"=b.",getObsIDColColname(dataf)))
+
+## Try to create a view instead of alter table
+#sqlSendUpdate(connection,paste0("create or replace view",tempoutput, "AS","select a from",outWideTbale,"a","getTable(dataf),"b\n"),
+#" set exposure=b.",getDepVarname(fit),"\n",
+#" where a.",getObsIDColColname(fit@deeptable),"=b.",getObsIDColColname(dataf))
 
 
 
-
-#scoretable<-table()
-
-FLtable<-function(table,
-                  obs_id_colname,
-                  var_id_colnames=character(0), 
-                  cell_val_colname=character(0),
-                  whereconditions=character(0),
-                  connection=getFLConnection(),
-                  type="double",
-                  fetchIDs=TRUE,
-                  ...)
-
-
-#view
-vviewName <- gen_view_name("matchit")
-sqlstr <- paste0("SELECT p.vectorValueColumn AS Num_Val
-                 FROM (",constructSelect(x),") AS p")
-q <- createView(vviewName,sqlstr)
-
+# 4. call FLMatchit 
 ret <- sqlStoredProc(connection,
                      "FLMatchIt",
-                     TableName = 'tblMatchItAlt',
-                     ObsIDColName = "PERSON_ID",
+                     TableName = outWideTable,
+                     ObsIDColName = "obs_id_colname",
                      TreatmentColName = "exposure",
-                     PropScoreCol = "prob",
-                     MatchOrderCol = "prob",
+                     PropScoreCol = "Y",
+                     MatchOrderCol = "Y",
                      TableOutput = 1,
                      outputParameter = c(OutTable = 'a')
 )
 
-
+## 5. constructing the result 
+##    1. call: see lm implementation 
+##    2. set formula to argument 
+##    3. set model to result of glm logistic regression 
+##    4. set match.matrix to NA 
+##    5. set discarded to FLVector with matchit results 
+##    6. set distance to NA 
+##    7. set weights to NA 
+##    8. set subclass an q.cut to same value as R matchit when doing nearest neighbor 
+##    9. set treat,X to formula parts (see lm implementation for ref) 
+##    10. reconstruct nn after instpecting R matchit results. 
+result <- matchit(FORM,Renv$dataf)
+str(result)
+structure(list(formula=FORM,
+    model=fit,
+    match.matrix="NA",
+    distance="NA",
+    weights="NA",
+    #subclass=
+    treat=FORM,
+    x=FORM
+    ),
+class="matchit")
+          
+          
+          sqlQuery(connection,"select * from A411737_UnMatched")
+          
+          
